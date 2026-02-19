e6f2c52c1f97b6fa2fc329fac44b043b577e90c3
Laboratorio Informática 3 - Pasto <labinf3.pasto@CAMPUS.local>
2026-02-19 07:41:14 -0500

    Ultimos cambios js

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 let darkmode = localStorage.getItem('darkmode');
-const themeSwitch = document.getElementById('theme-switch')
+const themeSwitch = document.getElementById('theme-switch');
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-}
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+};
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
-}
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+};
 
-if(darkmode === 'active') enableDarkmode()
+if (darkmode === 'active') enableDarkmode();
 
-themeSwitch.addEventListener("click", () => {
+themeSwitch.addEventListener('click', () => {
     darkmode = localStorage.getItem('darkmode');
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+    darkmode !== 'active' ? enableDarkmode() : disableDarkmode();
+});
 
-function vermas(dos){
-    const boton = document.getElementById("boton");
-    const contenido = document.getElementById(dos);
+const boton = document.getElementById('boton');
+const contenido = document.getElementById('dos');
 
-boton.addEventListener("click", function (){
-    if(contenido.style.display === "none"){
-        contenido.style.display = "block";
-        boton.textContent = "Ver menos";
-    }else{
-        contenido.style.display = "none";
-        boton.textContent = "Ver mas";
+boton.addEventListener('click', () => {
+    if (contenido.style.display === 'none') {
+        contenido.style.display = 'block';
+        boton.textContent = 'Ver menos';
+    } else {
+        contenido.style.display = 'none';
+        boton.textContent = 'Ver mas';
     }
-})
-}
+});
 
